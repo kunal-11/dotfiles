cb0d4428014fb66e327d337955204d9f4d579f89
panix <kunalmchoudhary@gmail.com>
2025-05-31 05:27:47 +0530

    cleanup

--- .config/hypr/pick_wallpaper.sh
+++ .config/hypr/pick_wallpaper.sh
@@ -1,4 +1,4 @@
 # Pick random wallpaper
-random_pick=$(find ~/wallpapers -type f -name "*.png" -o -name "*.jpg" | shuf -n1)
+random_pick=$(find ~/.wallpapers -type f -name "*.png" -o -name "*.jpg" | shuf -n1)
 # Replace the placeholder in template and save to .conf file
 sed -e "s~<wp>~${random_pick}~g" ~/.config/hypr/hyprpaper.template > ~/.config/hypr/hyprpaper.conf
