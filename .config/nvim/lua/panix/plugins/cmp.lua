31cb7a3582e6568e4fefc8ad696367a2dbec6753
Panix <panix@Panixs-MacBook-Pro.local>
2025-06-01 10:16:53 +0530

    transparent

--- .config/nvim/lua/panix/plugins/cmp.lua
+++ .config/nvim/lua/panix/plugins/cmp.lua
@@ -1,25 +1,13 @@
 return {
 	"saghen/blink.cmp",
 	opts = {
-		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-		-- 'super-tab' for mappings similar to vscode (tab to accept)
-		-- 'enter' for enter to accept
-		-- 'none' for no mappings
-		--
-		-- All presets have the following mappings:
-		-- C-space: Open menu or open docs if already open
-		-- C-n/C-p or Up/Down: Select next/previous item
-		-- C-e: Hide menu
-		-- C-k: Toggle signature help (if signature.enabled = true)
-		keymap = { preset = "default" },
+		keymap = { preset = "super-tab" },
 		signature = { enabled = true },
 
-		appearance = {
-			nerd_font_variant = "mono",
+		completion = {
+			ghost_text = { enabled = true, show_with_menu = false },
+			menu = { auto_show = false },
 		},
-
-		completion = { documentation = { auto_show = true } },
-
 		sources = {
 			default = { "lazydev", "lsp", "path", "snippets", "buffer" },
 			providers = {
