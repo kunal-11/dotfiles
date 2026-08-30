edf2fd65675bbc50f74cb1cedb42e87269784435
panix <panix>
2026-08-31 03:04:33 +0530

    move to sway

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,11 @@
+if status is-login
+	if test -z "$WAYLAND_DISPLAY"; and test "$XDG_VTNR" = 1
+		exec sway --unsupported-gpu
+	end
+end
+
 if status is-interactive
 end
 
-if status is-login 
-	and test (tty) = /dev/tty1
-        exec dbus-run-session river
-end
-
 alias bt=bluetoothctl
-
 starship init fish | source
